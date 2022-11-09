8133bc624c2dc53789177b4dad8a859b4af3d42e
Rustam Malikov <mrj@lowcost-mebel.ru>
2022-11-09 23:53:42 +0300

    ref: установить и удалить слушатель закрытия через ecs

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,16 +5,12 @@ export default class Popup {
   }
 
   open() {
-    document.addEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.addEventListener('keydown', this._handleEscClose.bind(this));
     this._popup.classList.add('popup_opened');
   }
 
   close() {
-    document.removeEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.removeEventListener('keydown', this._handleEscClose.bind(this));
     this._popup.classList.remove('popup_opened');
   }
 
