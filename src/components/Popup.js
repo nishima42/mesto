38b011cda0395a49394585366306ffd4fb7fa9f5
Rustam Malikov <mrj@lowcost-mebel.ru>
2022-11-10 14:23:19 +0300

    fix: исправить ссылки на метод

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,15 +2,16 @@ export default class Popup {
   constructor(popupSelector) {
     this._popupSelector = popupSelector;
     this._popup = document.querySelector(this._popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
     this._popup.classList.add('popup_opened');
   }
 
   close() {
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
     this._popup.classList.remove('popup_opened');
   }
 
